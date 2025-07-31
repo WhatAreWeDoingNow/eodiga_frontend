1118105bc141015111030555ee72d1f60472ad86
ansunghae <ahshss903@gmail.com>
2025-08-01 02:10:36 +0900

    FIX :: 페이지수 누적버그 수정2

--- src/pages/SignIn/Signin.jsx
+++ src/pages/SignIn/Signin.jsx
@@ -1,15 +1,17 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 /** @jsxImportSource @emotion/react */
 import { css } from '@emotion/react';
 import * as s from './style';
 import CommonPage from '../../components/CommonPage/CommonPage';
 import Header from '../../components/CommonPage/Header/Header';
 
-function Signin({page, setPage}) {
-     const savedEmails = ['user1@example.com', 'user2@example.com', 'admin@company.com'];
-  const [email, setEmail] = useState('');
+function Signin({ page, setPage }) {
+  useEffect(() => {
+    setPage(1); // 처음 진입 시에만 초기화
+  }, []);
 
-  page = 1; // 수정 전 코드 사용시 페이지수가 누적이 되는 버그가 있어 변수수정으로 해결.
+  const savedEmails = ['user1@example.com', 'user2@example.com', 'admin@company.com'];
+  const [email, setEmail] = useState('');
 
   const handleSelectChange = (e) => {
     const selectedEmail = e.target.value;
@@ -17,26 +19,22 @@ function Signin({page, setPage}) {
       setEmail(selectedEmail);
     }
   };
+
   return (
     <>
-        <CommonPage text={"다음"} maxpage={2} page={page} setPage={setPage}>
-          <Header
-            page={"signin"}
-            index={page}
-          />
-          <div css={s.inputbox}>
-              <input css={s.input} type="text" placeholder='example@'/>
-              <select css={s.select} name="" id="">
-                  <option  value="">이메일선택</option>
-                  <option  value="">@gmail.com</option>
-                  <option  value="">@naver.com</option>
-                  <option  value="">@daum.net</option>
-              </select>
-          </div>
-        </CommonPage>
+      <CommonPage text={"다음"} maxpage={2} page={page} setPage={setPage}>
+        <Header page={"signin"} index={page} />
+        <div css={s.inputbox}>
+          <input css={s.input} type="text" placeholder='example@' />
+          <select css={s.select} onChange={handleSelectChange}>
+            <option value="">이메일선택</option>
+            <option value="">@gmail.com</option>
+            <option value="">@naver.com</option>
+            <option value="">@daum.net</option>
+          </select>
+        </div>
+      </CommonPage>
     </>
-
-    
   )
 }
 
