b85fe834a138bf51790e32df15f5eb6e51acca68
ansunghae <ahshss903@gmail.com>
2025-08-06 00:19:09 +0900

    DESIGN :: Cardpage 디자인 수정

--- src/components/CardPage/style.js
+++ src/components/CardPage/style.js
@@ -4,6 +4,7 @@ export const container = css`
   position: relative;
   width: 100px;
   height: 100px;
+  margin-top: 10px;
 `;
 
 export const image = css`
@@ -15,11 +16,27 @@ export const image = css`
 
 export const title = css`
     width: 100px;
-    height: 30px;
+    height: 21px;
   position: absolute;
+  padding-top: 7px;
   top: 60px;
-  background-color: #a4a4a460  // 반투명 배경
+  background-color: #a4a4a460;
   color: black;
-  border-radius: 6px;
-  font-size: 14px;
+  border-radius: 0px 0px 14px 14px;
+  font-size: 12px;
+    color: #fff;
 `;
+
+export const location = css`
+  font-size: 10px;
+`
+
+export const star = css`
+  font-size: 10px;
+  margin-top: 1px;
+`
+
+export const container1 = css`
+  margin-top: -5px;
+  margin-left: 5px;
+`
