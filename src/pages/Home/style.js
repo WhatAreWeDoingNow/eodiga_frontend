51f9c58c37b1a4dce6fcc6a01d4c639d8104043c
hyeond0907 <kimjh597@naver.com>
2025-08-07 05:07:25 +0900

    last

--- src/pages/Home/style.js
+++ src/pages/Home/style.js
@@ -4,8 +4,6 @@ export const container = css`
     background-color: #FFA8A8;
     height: 100%;
     width: 100%;
-    display: flex;
-    flex-direction: column;
    `
 
 
---
+++ src/pages/Test/style.js
@@ -0,0 +1,41 @@
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react';
+
+export const container = css`
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    margin-top: 95px;
+    gap: 10px;
+    padding-left: 16px;;
+
+    p{
+        font-size: 17px;
+        font-weight: 400;
+        color: #FF6B6B;
+    }
+`
+
+export const aa = css`
+    display: flex;
+    gap: 5px;
+
+    span{
+        color: #929292;
+        font-size: 10px;
+        font-weight: 400;
+    }
+`
+
+export const a = css`
+    display: flex;
+    gap: 10px;
+
+    div {
+        padding: 5px 20px;
+        border-radius: 15px;
+        background-color: #FFA8A8;
+        font-size: 10px;
+        color: #FFF;
+    }
+`
